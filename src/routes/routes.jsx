d78bd4118d46d3ceee80961009d8a8fda601af48
003-demo <rrriku05@gmail.com>
2026-06-24 19:57:42 +0600

    first commit

---
+++ src/routes/routes.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-red-800 text-8xl">Hiiiiiiiiiiiiiii</div>,
+  },
+]);
