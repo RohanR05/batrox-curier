35c46e3ffd9ac18d573a725e3c8271c5fec85a39
003-demo <rrriku05@gmail.com>
2026-06-24 20:21:01 +0600

    added react router and set layout for home

--- src/RootLayout/RootLayout.jsx
+++ src/RootLayout/RootLayout.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+import { Outlet } from "react-router";
 
 const RootLayout = () => {
   return (
-    <div>
-      RootLayout
-    </div>
-  )
-}
+    <>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
