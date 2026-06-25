6f395831e7ce960dfc871e796860603cf59325a4
003-demo <rrriku05@gmail.com>
2026-06-25 23:07:32 +0600

    adding google fonts - bebas

--- src/RootLayout/RootLayout.jsx
+++ src/RootLayout/RootLayout.jsx
@@ -5,13 +5,13 @@ import { Outlet } from "react-router";
 
 const RootLayout = () => {
   return (
-    <>
+    <div className="bebas">
       <Navbar> </Navbar>
       <div className="max-w-7xl mx-auto my-2 md:my-4 lg:my-8">
         <Outlet></Outlet>
       </div>
       <Footer></Footer>
-    </>
+    </div>
   );
 };
 
