8bc3b62428bb8053e7a4147a0756fb1b52e55622
003-demo <rrriku05@gmail.com>
2026-06-27 23:05:45 +0600

    creat review card

--- src/pages/Home/Review/Review.jsx
+++ src/pages/Home/Review/Review.jsx
@@ -1,34 +1,34 @@
 import React, { use } from "react";
 import { EffectFlip, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import ReviewsCard from "../ReviewsCard/ReviewsCard";
 
 const Review = ({ reviewData }) => {
   const reviews = use(reviewData);
   console.log(reviews);
   return (
     <div>
-      {" "}
-      <>
-        <Swiper
-          effect={"coverflow"}
-          grabCursor={true}
-          slidesPerView={3}
-          centeredSlides={true}
-          coverflowEffect={{
-            rotate: "50",
-            stretch: "0",
-            depth: "100",
-            modifier: "1",
-            slideShadows: true,
-          }}
-          modules={[EffectFlip, Pagination]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/abstract-1.jpg" />
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        slidesPerView={3}
+        centeredSlides={true}
+        coverflowEffect={{
+          rotate: "50",
+          stretch: "0",
+          depth: "100",
+          modifier: "1",
+          slideShadows: true,
+        }}
+        modules={[EffectFlip, Pagination]}
+        className="mySwiper"
+      >
+        {reviews.map((review) => (
+          <SwiperSlide key={review.id}>
+            <ReviewsCard review={review}></ReviewsCard>
           </SwiperSlide>
-        </Swiper>
-      </>
+        ))}
+      </Swiper>
     </div>
   );
 };
