460c7f867d73baedb4da54fb48c476bb81c4f442
003-demo <rrriku05@gmail.com>
2026-06-28 21:35:21 +0600

    map

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const Coverage = () => {
+  const position = [23.6850, 90.6364];
   return (
     <div>
-      Coverage
+      <h2>map</h2>
+      <div className="w-full h-[1000px] border">
+        <MapContainer
+          className="w-full h-full"
+          center={position}
+          zoom={8}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+        ,
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Coverage
+export default Coverage;
