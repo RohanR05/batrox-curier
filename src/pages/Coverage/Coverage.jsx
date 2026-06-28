79b7f1260bedba20d3e609743d1a6202e01e22a9
003-demo <rrriku05@gmail.com>
2026-06-28 22:18:07 +0600

    done map

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,30 +1,78 @@
-import React from "react";
+import React, { useRef } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useLoaderData } from "react-router";
 
 const Coverage = () => {
-  const position = [23.6850, 90.6364];
+  const position = [23.685, 90.6364];
+  const data = useLoaderData();
+  const mapRef = useRef();
+
+  const handleSearchInput = (e) => {
+    e.preventDefault();
+    const location = e.target.search.value;
+    const district = data.find((c) =>
+      c.district.toLowerCase().includes(location.toLowerCase()),
+    );
+    if (district) {
+      const coor = [district.latitude, district.longitude];
+      mapRef.current.flyTo(coor, 12);
+    }
+  };
   return (
     <div>
-      <h2>map</h2>
-      <div className="w-full h-[1000px] border">
+      <h2 className="text-2xl font-medium my-4">
+        We are available in 64 district
+      </h2>
+      <form onSubmit={handleSearchInput} className="input mb-4">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input
+          type="search"
+          className="grow"
+          placeholder="Search"
+          name="search"
+        />
+      </form>
+      <div className="w-full h-[700px] border-3 border-secondary">
         <MapContainer
-          className="w-full h-full"
+          className="rounded-2xl w-full h-full"
           center={position}
           zoom={8}
           scrollWheelZoom={true}
+          ref={mapRef}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          {data.map((center, index) => {
+            return (
+              <Marker position={[center.latitude, center.longitude]}>
+                <Popup className="font-bold text-lg">
+                  <strong>District: {center.district}</strong>
+                  <p className="text-secondary text-lg">
+                    Service Area:{center.covered_area.join(", ")}
+                  </p>{" "}
+                </Popup>
+              </Marker>
+            );
+          })}
         </MapContainer>
-        ,
       </div>
     </div>
   );
