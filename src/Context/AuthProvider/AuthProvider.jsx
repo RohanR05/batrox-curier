3ee50654bec21b59a6526905e441ebbdf1d066c3
003-demo <rrriku05@gmail.com>
2026-07-02 23:55:30 +0600

    setting user state

--- src/Context/AuthProvider/AuthProvider.jsx
+++ src/Context/AuthProvider/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AuthContext } from "../AuthContext/AuthContext";
 import {
   createUserWithEmailAndPassword,
@@ -11,19 +11,27 @@ import { auth } from "../../firebase/firebase.init";
 const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   const registerUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const signInUser = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   const googleSignInUser = () => {
+    setLoading(true);
     return signInWithPopup(auth, googleProvider);
   };
 
   const authInfo = {
+    user,
+    loading,
     registerUser,
     signInUser,
     googleSignInUser,
