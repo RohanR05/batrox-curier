110a3746d49dcfe0ebdc2f4daceeee92c5d52389
003-demo <rrriku05@gmail.com>
2026-07-03 22:44:36 +0600

    create loading

---
+++ src/Components/Loading/Loading.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-4 bg-base-100">
+      <span className="loading loading-spinner loading-lg text-secondary"></span>
+
+      <h2 className="text-xl font-semibold text-secondary">
+        Loading...
+      </h2>
+
+      <p className="text-sm text-primary">
+        Please wait while we load your content.
+      </p>
+    </div>
+  );
+};
+
+export default Loading;
--- src/routes/PrivetRoutes.jsx
+++ src/routes/PrivetRoutes.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import useAuth from "../Hooks/useAuth";
 import { Navigate } from "react-router";
+import Loading from "../Components/Loading/Loading";
 
 const PrivetRoutes = (children) => {
   const { user, loading } = useAuth();
   if (loading) {
-    return <p>Loading</p>;
+    return <Loading></Loading>;
   }
   if (!user) {
     return <Navigate to={"/login"}></Navigate>;
