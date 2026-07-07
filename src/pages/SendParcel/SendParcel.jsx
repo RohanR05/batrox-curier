faae5281d7ee8c657d2f8191648b5a56bc2774ea
003-demo <rrriku05@gmail.com>
2026-07-07 21:48:20 +0600

    creaing parcel from

--- src/pages/SendParcel/SendParcel.jsx
+++ src/pages/SendParcel/SendParcel.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const SendParcel = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { error },
+  } = useForm();
+  const handleSendParcelForm = (data) => {
+    console.log(data);
+  };
   return (
     <div>
-      <h2>Send Parcel</h2>
+      {/* heading */}
+      <div>
+        <h2>Send Parcel</h2>
+        <p>Door to door delivery made easy</p>
+      </div>
+      {/* total form */}
+      <form onSubmit={handleSubmit(handleSendParcelForm)}>
+        <input type="text" />
+        <input type="submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default SendParcel
+export default SendParcel;
