969b310921cad43e3d06d1773092fac199f1cf29
003-demo <rrriku05@gmail.com>
2026-07-15 23:41:16 +0600

    get my parcels data by my email

--- src/pages/DashBoard/MyParcels/MyParcels.jsx
+++ src/pages/DashBoard/MyParcels/MyParcels.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useAuth from "../../../Hooks/useAuth";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const MyParcels = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: parcels = [] } = useQuery({
+    queryKey: ["myParcels", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels?email=${user.email}`);
+      return res.data;
+    },
+  });
   return (
     <div>
-      My Parcels
+      <h2>My Parcels</h2>
+      <p>Parcels Length: {parcels.length}</p>
     </div>
-  )
-}
+  );
+};
 
-export default MyParcels
+export default MyParcels;
