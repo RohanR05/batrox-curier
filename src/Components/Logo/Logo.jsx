7e6d31f54c1cf239b7f99ccc63bfdc214b1a1c5a
003-demo <rrriku05@gmail.com>
2026-07-16 23:42:02 +0600

    a

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -4,7 +4,9 @@ import { NavLink } from "react-router";
 const Logo = () => {
   return (
     <div className="text-xl font-bold">
-      <NavLink to={"/"}>BatRox Courier</NavLink>
+      <NavLink to={"/"}>
+        Bat<strong className="text-black">Rox</strong>
+      </NavLink>
     </div>
   );
 };
