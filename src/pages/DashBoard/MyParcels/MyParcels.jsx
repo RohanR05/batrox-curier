f74f13dc74f30050efc2586146b22fcfad4a2717
003-demo <rrriku05@gmail.com>
2026-07-18 23:12:05 +0600

    add action button on my parcels

--- src/pages/DashBoard/MyParcels/MyParcels.jsx
+++ src/pages/DashBoard/MyParcels/MyParcels.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import useAuth from "../../../Hooks/useAuth";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import { TbListDetailsFilled } from "react-icons/tb";
+import { MdAutoDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
 
 const MyParcels = () => {
   const { user } = useAuth();
@@ -26,7 +29,7 @@ const MyParcels = () => {
               <th>Parcel Title</th>
               <th>Parcel Cost</th>
               <th>Payment Status</th>
-              <th>Payment Action</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
@@ -37,7 +40,19 @@ const MyParcels = () => {
                 <th>{parcel.parcelTitle}</th>
                 <th>{parcel.cost}</th>
                 <th>Working</th>
-                <th>Working</th>
+                <th>
+                  <div className=" space-x-2">
+                    <button className="btn btn-square text-secondary hover:bg-secondary hover:text-white">
+                      <FaEdit />
+                    </button>
+                    <button className="btn btn-square text-green-600 hover:text-white hover:bg-green-600">
+                      <TbListDetailsFilled />
+                    </button>
+                    <button className="btn btn-square text-red-600 hover:bg-red-600 hover:text-white">
+                      <MdAutoDelete />
+                    </button>
+                  </div>
+                </th>
               </tr>
             ))}
           </tbody>
