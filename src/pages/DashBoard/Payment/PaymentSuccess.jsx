d73b242f7e6a06795874b38ab6cd8729cd3a0b99
003-demo <rrriku05@gmail.com>
2026-07-23 19:27:12 +0600

    show transaction id and tracking id

--- src/pages/DashBoard/Payment/PaymentSuccess.jsx
+++ src/pages/DashBoard/Payment/PaymentSuccess.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
@@ -6,6 +6,7 @@ const PaymentSuccess = () => {
   const [searchParams] = useSearchParams();
   const sessionId = searchParams.get("session_id");
   const axiosSecure = useAxiosSecure();
+  const [paymentInfo, setPaymentInfo] = useState({});
   console.log(sessionId);
 
   useEffect(() => {
@@ -14,10 +15,22 @@ const PaymentSuccess = () => {
         .patch(`payment-success?session_id=${sessionId}`)
         .then((res) => {
           console.log(res.data);
+          setPaymentInfo({
+            transactionId: res.data.transactionId,
+            trackingId: res.data.trackingId,
+          });
         });
     }
   }, [sessionId, axiosSecure]);
-  return <div>Payment Success</div>;
+  return (
+    <div>
+      Payment Success
+      <div>
+        <p>Transaction Id : {paymentInfo.transactionId}</p>
+        <p>Tracking ID : {paymentInfo.trackingId}</p>
+      </div>
+    </div>
+  );
 };
 
 export default PaymentSuccess;
